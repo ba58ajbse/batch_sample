679f61e459f3f50c68c8b19bafdccdaa92ad1f6c
Administrator <admin@example.com>
2021-08-11 14:41:23 +0900

    refactoring update.rake

---
+++ app/models/rank_order_maker.rb
@@ -0,0 +1,27 @@
+class RankOrderMaker
+  def each_ranked_user
+    rank = 1
+    previous_score = 0
+
+    user_sorted_by_score.each do |user|
+      # 合計スコアが変わった場合は順位を更新
+      rank += 1 if user.total_score < previous_score
+
+      # app/models/ranks_update.rb の create_ranks に書いた
+      # Rank.create(user_id: user.id, rank: rank, score: user.total_score) を実行
+      yield(user, rank)
+
+      # previous_score を更新して、次の順位更新に備える
+      previous_score = user.total_score
+    end
+  end
+
+  private
+
+  def user_sorted_by_score
+    User.all
+      .select { |user| user.total_score.nonzero? }
+      .sort_by { |user| user.total_score * -1 }
+  end
+
+end
